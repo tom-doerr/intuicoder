3a20fbc0e8711e0d73eba6b9203759a35857f41c
Tom Dörr <tomdoerr96@gmail.com>
2024-05-11 12:54:57 +0200

    Add code generation instructions and error handling.

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ EXCLUDE_DOT_FILES = True
 
 @function
 def generate_code(s, code_before_with_line_numbers, code_after_with_line_numbers, instruction):
+    s += "You don't need to adhere to the line numbers and can generate as much or as little code as you want. The line numbering is just for the current version of the code you are supposed to modify.\n"
     s += "It's your task to write code following the following instruction:\n"
     s += "Instruction: " + instruction + "\n"
     s += "Code after the instruction:\n"
@@ -51,7 +52,8 @@ def generate_code(s, code_before_with_line_numbers, code_after_with_line_numbers
     s += "```python\n"
     s += gen("code_with_line_numbers", max_tokens=1024, stop=["```", "\nassert"])
     s += "```\n\n"
-    s += "Same code in between the two code blocks but without line numbers:\n"
+    s += "Same code in between the two code blocks but without line numbers and without duplicate lines at the start and at the end:\n"
+    # s += "Same code in between the two code blocks but without line numbers and without duplicate lines:\n"
     s += "```python\n"
     s += gen("code", max_tokens=1024, stop=["```", "\nassert"])
 
@@ -127,6 +129,7 @@ def add_line_numbers(text, start_line_number):
 def insert_code(file_path, code, line_number):
     with open(file_path, 'r') as f:
         f_content = f.read()
+        f.flush()
 
     lines = f_content.split('\n')
     lines.insert(line_number, code)
@@ -135,16 +138,90 @@ def insert_code(file_path, code, line_number):
     with open(file_path, 'w') as f:
         f.write(new_content)
 
+ASSERT_ID_MESSAGE = 'dummy error message to check if statement is true'
+
 import subprocess
 def run_program():
     run_command = run_config.config['run_command']
     print("run_command:", run_command)
-    process = subprocess.Popen(run_command, shell=True, stdout=subprocess.PIPE)
+    process = subprocess.Popen(run_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process.wait()
-    print("Output:")
-    print(process.stdout.read())
-    exit_code = process.returncode
-    return exit_code == 0
+    # print("Output:")
+    # print(process.stdout.read())
+    # if process.stderr:
+        # out_err = process.stderr.read()
+        # print("out_err_2:", out_err)
+    stdout, stderr = process.communicate()
+    if stdout:
+        print("stdout:", stdout.decode())
+    if stderr:
+        # print("stderr:", stderr.decode())
+        stderr_str = stderr.decode()
+        print("stderr_str:", stderr_str)
+        if f'AssertionError: {ASSERT_ID_MESSAGE}' in stderr_str:
+            return True
+
+    # exit_code = process.returncode
+    # return exit_code == 0
+    return False
+
+def add_assert_check(file_content, assert_line_number):
+    # add ASSERT_ID_MESSAGE to the assert statement and invert the condition
+    lines = file_content.split('\n')
+    assert_line = lines[assert_line_number]
+    assert_line = assert_line.replace('assert', 'assert not') + f', "{ASSERT_ID_MESSAGE}"'
+    lines[assert_line_number] = assert_line
+    return '\n'.join(lines)
+
+def remove_assert_check(file_content, assert_line_number):
+    lines = file_content.split('\n')
+    assert_line = lines[assert_line_number]
+    assert_line = assert_line.replace('assert not', 'assert')
+    assert_line = assert_line.replace(f', "{ASSERT_ID_MESSAGE}"', '')
+    lines[assert_line_number] = assert_line
+    return '\n'.join(lines)
+
+
+def get_line_number_assert_id_message(file_content):
+    lines = file_content.split('\n')
+    for i, line in enumerate(lines):
+        if ASSERT_ID_MESSAGE in line:
+            return i
+
+    assert False, "No assert id message found"
+
+def remove_assert_check_file(file_path):
+    print('============= remove_assert_check_file')
+    with open(file_path, 'r') as f:
+        f_content = f.read()
+
+    print("f_content:", f_content)
+    print('='*50)
+    assert_line_number = get_line_number_assert_id_message(f_content)
+    f_content = remove_assert_check(f_content, assert_line_number)
+    print("f_content:", f_content)
+    print('='*50)
+    with open(file_path, 'w') as f:
+        f.write(f_content)
+        f.flush()
+
+def comment_out_instruction(file_path, instruction_line_number):
+    print('============== comment_out_instruction')
+    with open(file_path, 'r') as f:
+        file_content = f.read()
+    print("file_content:", file_content)
+    print('='*50)
+    lines = file_content.split('\n')
+    lines[instruction_line_number] = '#' + lines[instruction_line_number]
+    new_content = '\n'.join(lines)
+    print("new_content:", new_content)
+    print('='*50)
+    with open(file_path, 'w') as f:
+        f.write(new_content)
+        f.flush()
+
+
+
 
 def main():
     attempt = 0
@@ -176,6 +253,9 @@ def main():
         with open(file_path, 'r') as f:
             original_file_content = f.read()
 
+        f_content = add_assert_check(f_content, assert_line_number)
+        with open(file_path, 'w') as f:
+            f.write(f_content)
         insert_code(file_path, code, instruction_line_number + 1)
         success = run_program()
         print("success:", success)
@@ -185,6 +265,8 @@ def main():
                 f.write(original_file_content)
             print("Code reverted to original")
         else:
+            comment_out_instruction(file_path, instruction_line_number)
+            remove_assert_check_file(file_path)
             print(f"Code successfully inserted after {attempt} attempts")
             break
 
--- start_server.sh
+++ start_server.sh
@@ -4,4 +4,5 @@ export CUDA_VISIBLE_DEVICES=0,1
 #python3 -m sglang.launch_server --model-path liuhaotian/llava-v1.6-vicuna-7b --tokenizer-path llava-hf/llava-1.5-7b-hf --chat-template vicuna_v1.1 --port 30813 --tp 2
 
 #python3 -m sglang.launch_server --model-path MaziyarPanahi/Meta-Llama-3-70B-Instruct-GPTQ --port 38242 --tp 2
-python3 -m sglang.launch_server --model-path TechxGenus/Meta-Llama-3-70B-GPTQ --port 38242 --tp 2
+#python3 -m sglang.launch_server --model-path TechxGenus/Meta-Llama-3-70B-GPTQ --port 38242 --tp 2
+python3 -m sglang.launch_server --model-path TechxGenus/Meta-Llama-3-8B-GPTQ --port 38242
